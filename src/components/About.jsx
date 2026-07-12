18ed5df2c553a27f0e3cd527770eb87caf6ce775
Wesley Padilha <wtpadilhaa@gmail.com>
2026-07-12 15:32:11 -0300

    Alteração de texto home

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -7,16 +7,21 @@ function About() {
       <h2>Sobre</h2>
 
       <p>
-        Wesley Padilha é DJ de Tribal House, conhecido por sets
-        energéticos e uma identidade sonora marcante.
-        Sua performance mistura batidas tribais, grooves intensos
-        e uma atmosfera criada para a pista.
+        Wesley Padilha é DJ de Tribal House, Edm e House Music,
+        atualmente residente em Curitiba/PR, está a 2 anos na cena, com parceria
+        fixa com o selo After House, segue em uma crescente
+        se consilando cada vez mais na cena eletrônica, para alcançar novos horizontes, iniciou
+        seus estudos em produção musical e já promete grandes projetos para a cena atual,
+        conhecido por sets
+        animados e uma identidade sonora marcante.
+        Sua performance mistura emoção, grooves intensos
+        e uma experiência criada para a pista.
       </p>
 
       <p>
-        Já passou por clubs e eventos como Versus Club, Dunkel,
-        Bar do Deca, Eclipse Tubarão e Criciúma, Don Club,
-        Vitto Bar e Espaço Central (After House).
+        Já passou por clubs e eventos nas grandes capitais e tem circulado o Sul
+        marcando presença em Curitiba nos clubs: Versus Club, Dunkel,
+        Vitto Bar, Cavan77 e Espaço Central, e na grande Florianópolis: Bar do Deca, Eclipse Tubarão e Criciúma, Don Club.
       </p>
 
     </section>
